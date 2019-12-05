15a033612fc9a19f47d6a95781f3b53f81f867f7
Monsterbolt101 <kerr34459@gmail.com>
2019-12-04 18:28:21 -0600

    Fixed headless issue modified:   run.jar modified:   src/net/mcorp/networked/interfaces/server/ServerController.java

--- src/net/mcorp/networked/interfaces/server/ServerController.java
+++ src/net/mcorp/networked/interfaces/server/ServerController.java
@@ -1,25 +1,42 @@
 package net.mcorp.networked.interfaces.server;
 
+import java.awt.HeadlessException;
+
 import javax.swing.*;
 
 public class ServerController<ServerHandler_ extends ServerHandler> {
 
+	@SuppressWarnings("unused")
 	private final JFrame frame;
+	
+	@SuppressWarnings("unused")
 	private final JPanel root;
 	
-	private Server<ServerHandler_> server;
+	private final boolean headless;
+	public final boolean isHeadless() { return this.headless; };
+	
+	public final Server<ServerHandler_> server;
 	
 	protected ServerController(Server<ServerHandler_> server) {
 		this.server = server;
-		this.frame = new JFrame();
-		this.frame.setTitle("Server Controller");
-		this.frame.setSize(720, 480);
-		
-		this.root = new JPanel(null);
-		
-		this.frame.add(this.root);
-		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.frame.setVisible(true);
+		JFrame frame = null;
+		JPanel root = null;
+		boolean headless;
+		try {
+			frame = new JFrame();
+			frame.setTitle("Server Controller");
+			frame.setSize(720, 480);
+			
+			root = new JPanel(null);
+			
+			frame.add(root);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setVisible(true);
+			headless = false;
+		}catch(HeadlessException e) { headless = true; }
+		this.headless = headless;
+		this.frame = frame;
+		this.root = root;
 	}
 
 }
